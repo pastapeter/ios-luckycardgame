34324f9668a375f76c09ddc0bb3c6923be04ea6b
Dohyun Chung <pastapeter@naver.com>
2023-07-04 12:07:40 +0900

    [Add] project 추가, iOS minTarget 15.0

---
+++ LuckyCardGame/LuckyCardGame/View/GameBoardViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameBoardViewController.swift
+//  LuckyCardGame
+//
+//  Created by Jung peter on 7/4/23.
+//
+
+import UIKit
+
+class GameBoardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
