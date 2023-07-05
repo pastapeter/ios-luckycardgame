07a2d7edb30c07bb9a8e9fe7b8ca08ae076f7f74
Dohyun Chung <pastapeter@naver.com>
2023-07-05 17:46:29 +0900

    [Add] some 을 사용하여, random card 만들기

--- LuckyCardGame/LuckyCardGame/Model/LuckyCardMaker.swift
+++ LuckyCardGame/LuckyCardGame/Model/LuckyCardMaker.swift
@@ -10,10 +10,25 @@ import Foundation
 /// 내부에 프로퍼티가 존재할 필요 없다.
 /// func 자체가 공유해야하는 것도 없다. -> struct
 struct LuckyCardMaker {
+  
+  static func makeIntCardValue() -> some CardValuable {
+    let intDb: [Int] = [1,2,3,4,5,6,7,8,9,10,11,12]
+    return intDb[Int.random(in: (0..<intDb.count))]
+  }
+  
+  static func makeStrCardValue() -> some CardValuable {
+    let strDb: [String] = ["K", "Q", "J" ]
+    return strDb[Int.random(in: (0..<strDb.count))]
+  }
+  
   static func generateRandomly() -> LuckyCard? {
-    let db: [any CardValuable] = [1,2,3,4,5,6,7,8,9,10,11,12, "K", "Q", "J"]
+    let randChoice = Int.random(in: (0...1))
     do {
-      return try LuckyCard(type: CardType.allCases[Int.random(in: (0...2))], value: db[Int.random(in: (0..<db.count))])
+      if randChoice == 0 {
+        return try LuckyCard(type: CardType.allCases[Int.random(in: (0...2))], value: makeIntCardValue())
+      } else {
+        return try LuckyCard(type: CardType.allCases[Int.random(in: (0...2))], value: makeStrCardValue())
+      }
     } catch(let e){
       print("Generate has Error with \(e.localizedDescription) so Goto Next iteration")
       return nil
