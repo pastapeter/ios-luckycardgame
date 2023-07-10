35b442181a25e7bbdef84affae8a6837376db697
Dohyun Chung <pastapeter@naver.com>
2023-07-10 13:59:52 +0900

    [Feat] Field카드가 존재한다. Field에 존재해야하는 카드를 모아둔다.

---
+++ LuckyCardGame/LuckyCardGame/Model/Field/LuckyGameField.swift
@@ -0,0 +1,27 @@
+//
+//  LuckyGameField.swift
+//  LuckyCardGame
+//
+//  Created by Jung peter on 7/10/23.
+//
+
+import Foundation
+
+class LuckyGameField: CardReceivable {
+  
+  typealias CardDeck = LuckyCardDeck
+  var deck: CardDeck
+  
+  init(deck: CardDeck) {
+    self.deck = deck
+  }
+  
+  func receiveCard(_ card: LuckyCard) {
+    do { try deck.add(card: card) }
+    catch (let e) { print(e.localizedDescription) }
+  }
+  
+  convenience init() {
+    self.init(deck: LuckyCardDeck.make())
+  }
+}
