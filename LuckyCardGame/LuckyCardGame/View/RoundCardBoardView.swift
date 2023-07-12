34341c513a7ff9c1b231d9691f7be2ba6709b562
Dohyun Chung <pastapeter@naver.com>
2023-07-12 16:40:28 +0900

    [Fix] 카드뷰, 카드Board 뷰 모두 동일한 custom view 상속받고있었는데, 특정 부분에서 공통기능이 달라져서 객체를 나누고 상속형태로 변경

--- LuckyCardGame/LuckyCardGame/View/RoundCardBoardView.swift
+++ LuckyCardGame/LuckyCardGame/View/RoundCardBoardView.swift
@@ -7,29 +7,4 @@
 
 import UIKit
 
-class RoundCardBoardView: UIView, CardAddable, CardRemovable {
-  
-  init(frame: CGRect, radius: Int) {
-    super.init(frame: frame)
-    setRound(radius: radius)
-  }
-  
-  required init?(coder: NSCoder, radius: Int) {
-    super.init(coder: coder)
-    setRound(radius: radius)
-  }
-  
-  convenience override init(frame: CGRect) {
-    self.init(frame: frame, radius: 16)
-  }
-  
-  convenience required init?(coder: NSCoder) {
-    self.init(coder: coder, radius: 16)
-  }
-  
-  func setRound(radius: Int) {
-    self.layer.cornerRadius = CGFloat(radius)
-    self.layer.masksToBounds = true
-  }
-  
-}
+class RoundCardBoardView: RoundView, CardAddable, CardRemovable { }
